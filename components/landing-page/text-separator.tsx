a1a6829565e050491386c3c5e379d0872145a2fc
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2025-06-02 21:09:55 -0300

    teste

--- components/landing-page/text-separator.tsx
+++ components/landing-page/text-separator.tsx
@@ -15,9 +15,9 @@ export function SecondSeparator() {
     return (
         <div className="w-full bg-[url('/secondseparatorbg.png')] bg-cover bg-center bg-no-repeat h-[25vh] flex items-center justify-center">
             <div className="text-white text-3xl md:text-5xl font-heebo font-bold text-center drop-shadow-lg flex flex-col">
-                Possuímos membros em{" "}
+             
                 <span className="text-yellow-400 font-bold">
-                    mais de 7 porras pelo mundo.
+                    
                 </span>
             </div>
         </div>
